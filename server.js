946ef0bd4f9add11937a9b75d07ab163f84ddbde
Muhammed nasser <mohamedabdelnasser37@gmail.com>
2022-10-24 23:31:59 +0200

    init

---
+++ server.js
@@ -0,0 +1,16 @@
+const path = require('path');
+const express = require('express');
+
+const app = express();
+
+
+// Setting static folders
+app.use(express.static(path.join(__dirname,'public')));
+
+
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT , ()=>{
+    console.log(`Server started on port : ${PORT}`);
+});
