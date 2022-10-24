d5d2a58bcc08656d36a7b82f57f5630d41ab1ae9
Muhammed nasser <mohamedabdelnasser37@gmail.com>
2022-10-24 23:53:33 +0200

    emit messags if user joined or left the chat

--- server.js
+++ server.js
@@ -1,16 +1,39 @@
 const path = require('path');
+const http = require('http');
 const express = require('express');
+const socketio = require('socket.io');
 
 const app = express();
+const server = http.createServer(app);
+const io = socketio(server);
 
 
 // Setting static folders
 app.use(express.static(path.join(__dirname,'public')));
 
+// when user connect
+io.on('connection', (socket) => {
+    console.log('New web server connection...');
+
+    // When user connect
+    socket.emit('message', 'welcome to the Chat');
+
+    // Notify users there is new user connected
+    socket.broadcast.emit('message', 'A user has joind the chat');
+
+    // Notify users there is a user disconnected
+    
+    socket.on('disconnect', ()=>{
+        
+        io.emit('message', 'A user has left the chat');
+    });
+
+
+});
 
 
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT , ()=>{
+server.listen(PORT , ()=>{
     console.log(`Server started on port : ${PORT}`);
 });
